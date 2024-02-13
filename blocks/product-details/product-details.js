8ac79b3ffab737fd3cbbb772efb5b3d2ba4d1e20
Stephen Rugh <rugh@adobe.com>
2024-02-13 16:02:29 -0600

    exercise 2 solution
    Signed-off-by: Stephen Rugh <rugh@adobe.com>

--- blocks/product-details/product-details.js
+++ blocks/product-details/product-details.js
@@ -43,13 +43,64 @@ export default async function decorate(block) {
       controls: 'thumbnailsColumn', // 'thumbnailsColumn', 'thumbnailsRow', 'dots'
       mobile: true,
     },
-    onAddToCart: (args) => {
-      // eslint-disable-next-line no-console
-      console.debug('onAddToCart', args);
-      addProductsToCart([args]);
-    },
+    hideAttributes: true,
+    hideDescription: false,
     slots: {
-      // ???? 🔮 What could this be?
+      Title: (ctx) => {
+        const category = document.createElement('div');
+        category.classList.add('pdp-tagline');
+        category.innerText = 'A WKND Classic';
+        ctx.prependSibling(category);
+      },
+      Quantity: (ctx) => {
+        const label = document.createElement('div');
+        label.classList.add('pdp-quantity-label');
+        label.innerText = 'Quantity';
+        ctx.prependChild(label);
+      },
+      Actions: (ctx) => {
+        ctx.appendButton((next) => ({
+          text: 'ADD TO CART',
+          icon: 'Cart',
+          variant: 'primary',
+          disabled: !next.data?.inStock || !next.valid,
+          onClick: async () => {
+            await addProductsToCart([
+              {
+                ...next.values,
+              },
+            ]);
+            window.location.href = '/cart';
+          },
+        }));
+
+        ctx.appendButton((next) => ({
+          'aria-label': 'Add to Wishlist',
+          icon: 'Heart',
+          variant: 'primary',
+          disabled: !next.valid,
+          onClick: () => {
+            // eslint-disable-next-line no-console
+            console.log('Add To Wishlist', next.values);
+          },
+        }));
+      },
+      GalleryContent: (ctx) => {
+        const el = document.createElement('div');
+        el.className = 'custom-content-section';
+
+        const titleEl = document.createElement('strong');
+        titleEl.textContent = 'Product Details';
+        el.appendChild(titleEl);
+
+        ctx.data.attributes.forEach(({ label, value }) => {
+          const attributeEl = document.createElement('li');
+          attributeEl.textContent = `${label}: ${value}`;
+          el.appendChild(attributeEl);
+        });
+
+        ctx.appendChild(el);
+      },
     },
   })(block);
 }
